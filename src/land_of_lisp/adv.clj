d7ca8a8f9e2d96974b1ce53cb0ed7013a15df402
Tsuyoshi Kawasaki <sambatriste@gmail.com>
2015-11-15 00:18:10 +0900

    hoge

--- src/land_of_lisp/adv.clj
+++ src/land_of_lisp/adv.clj
@@ -2,46 +2,45 @@
 
 
 
-(def ^:dynamic *nodes*
-  {:living-room "you are in the living room. a wizard is snoring loudly on the couch."
-   :garden      "you are in a beautiful garden. there is a well in front of you."
-   :attic       "you are in the attic. there is a giant welding torch in the corner."
-   })
 
-(def aaa {:living-room '(you are in the living room. a wizard is snoring loudly on the couch.)
-            :garden    '(you are in a beautiful garden. there is a well in front of you.)
-            :attic     '(you are in the attic. there is a giant welding torch in the corner.)})
+(def nodes '{living-room (you are in the living room. a wizard is snoring loudly on the couch.)
+            garden      (you are in a beautiful garden. there is a well in front of you.)
+            attic       (you are in the attic. there is a giant welding torch in the corner.)})
 
-(defn stringify
-  [lst]
-  (clojure.string/join " " (map name lst)))
+(def edges
+  '{living-room [[garden west door]
+                 [attic upstair ladder]]
+   garden      [[living-room east door]]
+   attic       [[living-room downstair ladder]]})
 
-(defn describe-loc
+(def objects '[whiskey bucket frog chain])
+(def object-locations
+  '{whiskey living-room
+    bucket  living-room
+    chain   garden
+    frog    garden})
+(defn- conc [lst] (clojure.string/join " " lst))
+
+(defn- stringify [lst] (conc (map name lst)))
+
+(defn describe-location
   [loc nodes]
   (stringify (get nodes loc)))
 
-
-
-(defn describe-location
-  [location-to-desc nodes]
-  (get nodes location-to-desc))
-
-
-(def ^:dynamic *edges*
-  {:living-room [["garden" "west" "door"]
-                 ["attic" "upstair" "ladder"]]
-   :garden      [["living-room" "east" "door"]]
-   :attic       [["living-room" "downstair" "ladder"]]})
-
-
 (defn describe-path
   [edge]
-  (clojure.string/join
-    " "
-    ["there is a" (nth edge 2) "going" (nth edge 1) "from here."]))
+  (stringify `(there is a ~(nth edge 2) going ~(nth edge 1) from here.)))
 
-(defn describe-paths [location edges]
-  (clojure.string/join
-    " "
-    (map describe-path (get edges location))))
 
+(defn describe-paths
+  [location edges]
+  (conc (map describe-path (get edges location))))
+
+
+(defn objects-at [loc objs obj-locs]
+  (letfn [(at-loc? [obj] (= (get obj-locs obj) loc))]
+    (filter at-loc? objs)))
+
+(defn describe-objects [loc objs obj-loc]
+  (letfn [(describe-obj [obj] (stringify `(you see a ~obj on the floor.)))]
+    (conc (map describe-obj (objects-at loc objs obj-loc)))))
--- test/land_of_lisp/adv_test.clj
+++ test/land_of_lisp/adv_test.clj
@@ -2,31 +2,24 @@
   (:require [clojure.test :refer :all]
             [land-of-lisp.adv :refer :all]))
 
-(deftest aaa-test
-  (let [v (get aaa :living-room)]
-    (is (= (clojure.string/join " " (map name v))
-           "you are in the living room. a wizard is snoring loudly on the couch."))))
-
-(deftest nodes-test
-  (is (= (:living-room *nodes*)
-         "you are in the living room. a wizard is snoring loudly on the couch.")))
 
 (deftest describe-location-test
-  (is (= (describe-location :attic *nodes*)
+  (is (= (describe-location 'attic nodes)
          "you are in the attic. there is a giant welding torch in the corner.")))
-
-
-
-(deftest describe-loc-test
-  (is (= (describe-loc :attic aaa)
-         "you are in the attic. there is a giant welding torch in the corner.")))
-
 
 
 (deftest describe-path-test
-  (is (= (describe-path ["garden" "west" "door"])
+  (is (= (describe-path '[garden west door])
          "there is a door going west from here.")))
 
 (deftest describe-paths-test
-  (is (= (describe-paths :living-room *edges*)
+  (is (= (describe-paths 'living-room edges)
          "there is a door going west from here. there is a ladder going upstair from here.")))
+
+(deftest objects-at-test
+  (is (= (objects-at 'living-room objects object-locations)
+         '(whiskey bucket))))
+
+(deftest describe-objects-test
+  (is (= (describe-objects 'living-room objects object-locations)
+         "you see a whiskey on the floor. you see a bucket on the floor.")))
