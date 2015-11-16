d77b474ac836755baf24e13c1476271df096fb3f
Tsuyoshi Kawasaki <sambatriste@gmail.com>
2015-11-16 22:57:36 +0900

    hoge

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject land-of-lisp "0.1.0-SNAPSHOT"
+(defproject land-of-clojure "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
