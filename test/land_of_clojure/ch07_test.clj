06ad58bd4245c12a7af0e04a8c035444681cdef3
KAWASAKI Tsuyoshi <sambatriste@gmail.com>
2016-02-02 08:31:32 +0900

    hoge

--- test/land_of_clojure/ch07_test.clj
+++ test/land_of_clojure/ch07_test.clj
@@ -12,6 +12,7 @@
   (is (= (dot-name '$ABC!) "_ABC_")))
 
 (deftest chop-test
+  "指定した文字数以上だと切り詰められること"
   (is (= (chop "abc" 30) "abc"))
   (is (= (chop "0123456789012345678901234567890123456789" 30)
          "012345678901234567890123456...")))
@@ -22,14 +23,42 @@
   (is (nil? (dot-label))))
 
 (deftest nodes->dot-test
-  (is (nil? (nodes->dot land-of-clojure.ch05/*nodes*))))
+  (is (= (with-out-str (nodes->dot land-of-clojure.ch05/*nodes*))
+         (str
+          "\n"
+          "living_room[label=\"(living-room you are in the ...)\"];\n"
+          "garden[label=\"(garden you are in a beautif...)\"];\n"
+          "attic[label=\"(attic you are in the attic....)\"];"))))
 
-(doseq [edge land-of-clojure.ch05/*edges*]
-  (let [from (first edge)
-        dests (rest edge)]
-    (doseq [dest dests] ;; カッコが１こ余計にあるのではずす
-      ;;(println dest)
-      (doseq [e dest]
-        (print (str (dot-name from) "->" ) )
-        (println (str (first e) "[label=\"" (rest e) "\""))))))
-        
+(deftest edges->dot-test
+  (is (= (with-out-str (edges->dot land-of-clojure.ch05/*edges*))
+         (str
+          "\n"
+          "living_room->garden[label=\"(west door)\"]\n"
+          "living_room->attic[label=\"(upstair ladder)\"]\n"
+          "garden->living-room[label=\"(east door)\"]\n"
+          "attic->living-room[label=\"(downstair ladder)\"]\n"
+          ))))
+
+(deftest graph->dot-test
+  (is (= (with-out-str (graph->dot land-of-clojure.ch05/*nodes*
+                                   land-of-clojure.ch05/*edges*))
+         (str
+          "digraph{\n"
+          "living_room[label=\"(living-room you are in the ...)\"];\n"
+          "garden[label=\"(garden you are in a beautif...)\"];\n"
+          "attic[label=\"(attic you are in the attic....)\"];\n"
+          "living_room->garden[label=\"(west door)\"]\n"
+          "living_room->attic[label=\"(upstair ladder)\"]\n"
+          "garden->living-room[label=\"(east door)\"]\n"
+          "attic->living-room[label=\"(downstair ladder)\"]\n"
+          "}"
+          ))))
+
+(dot->png
+ "test.png"
+ (fn []
+   (graph->dot land-of-clojure.ch05/*nodes*
+               land-of-clojure.ch05/*edges*)))
+
+
